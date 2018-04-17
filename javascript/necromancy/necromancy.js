00657c06567f4602ce6434adbc2ffe2f36f9a1a5
nyhilo <silentclowd@gmail.com>
2018-04-16 20:29:51 -0700

    Laying out the general structure of the Game object

--- javascript/necromancy/necromancy.js
+++ javascript/necromancy/necromancy.js
@@ -27,7 +27,10 @@
  // Helper Functions //
 //////////////////////
 
-function $(id) {return document.getElementById(id);}
+function $(id) {return document.getElementById(id);}    // ex $('bonecount')
+
+function floor(num) {return Math.floor(num);}           // Rounds down to nearest decimal
+function floor2(num) {return Maith.floor(num*100)/100;} // Rounds down to nearest hundredth's place
 
 
   //////////////////////
@@ -37,5 +40,84 @@ function $(id) {return document.getElementById(id);}
 let Game = {} 
 
 Game.Launch = function() {
-    //
+    // Constants
+    const Game.fps = 30;
+
+    // Variables
+    Game.eventTimer = 0;    // For longer-than-one-frame operations
+
+    // Currencies
+    var curr = {
+        bones:0,
+        gold:0,
+        mana:0,
+        maxMana:100
+    }
+
+    // Template for a unit
+    class Unit {
+        constructor(name, c, b=0, g=0, m=0) {    // Constructor takes all incrementors as 0 by default
+            this.name = name;
+            this.cost = c;
+            this.bpsmod = b;
+            this.gpsmod = g;
+            this.mpsmod = m;
+            this.pop = 0;
+            this.employed = 0;
+        }
+    }
+
+    // Units
+    var units {
+        skeleton: new Unit("skeleton", 10, b=.1, g=.1, m=0),
+        skelArmy: new Unit("skelArmy", 100, b=1.0, g=1.0, m=0)
+    }
+
+    // A template for a job containing many workers
+    class job {
+        constructor() {
+            // Number of each unit current eployed to this job
+            this.skeletons = 0;
+            this.skelArmies = 0;
+
+            this.bps = 0;
+            this.gps = 0;
+            this.mps = 0;
+        }
+
+        tallyStats() {
+            this.bps = ((this.skeletons * units.skeleton.bpsmod) +
+                       (this.skelArmies * units.skelArmy.bpsmod));
+
+            this.gps = ((this.skeletons * units.skeleton.gpsmod) +
+                       (this.skelArmies * units.skelArmy.gpsmod * 1.2));
+
+            this.mps = ((this.skeletons * units.skeleton.mpsmod) +
+                       (this.skelArmies * units.skelArmy.mpsmod));
+        }
+    }
+
+    var jobs = {
+        boneDigger: new job(),
+        travelerAttacker: new job()
+    }
+
+    // Incrementals
+    var incr = {
+        bps:0,      // Bones per second
+        gps:0,      // Gold per second
+        mps:1       // Mana recharge per second
+    }
+
+    Game.Logic = function() {
+
+    }
+
+    Game.Loop = function() {
+        // Do some stuff
+        Game.Logic();
+
+        Game.eventTimer++;
+        setTimeout(Game.Loop, 1000/Game.fps);
+    }
 }
