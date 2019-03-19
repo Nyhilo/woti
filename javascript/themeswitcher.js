f4570b237b52ede151f3d33d2f4014d25dd42e10
JakeCBlake <jacclowa@gmail.com>
2019-03-19 11:46:37 -0700

    Added cookie for dark mode persistence.

--- javascript/themeswitcher.js
+++ javascript/themeswitcher.js
@@ -3,13 +3,19 @@
 	var $themeLink = $("#dark-css");
 	var themeLocation = "css/dark.css";
 	var $themeButton = $('a.theme-switcher');
+	var themecookie = Tools.getCookie("theme");
+
+	if (themecookie != "" || themecookie != null)
+		$themeLink.attr("href", themecookie);
 
 	$themeButton.click(function() {
 		if ($themeLink.attr("href") == themeLocation) {
 			$themeLink.attr("href", "");
+			Tools.setCookie("theme", "default", 0);
 		}
 		else {
 			$themeLink.attr("href", themeLocation);
+			Tools.setCookie("theme", themeLocation, 9999);
 		}
 	})
 })();
